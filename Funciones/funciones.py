c30d6e79d1fa8a26113eb5169b6a8574825e0888
KeSuarezAr <kesuarezar@uide.edu.ec>
2022-11-15 10:31:56 -0500

    actividadclase

---
+++ Funciones/funciones.py
@@ -0,0 +1,4 @@
+def saludo():
+    return "Hello World!"
+
+print(saludo())
---
+++ Funciones/funcionesParametros.py
@@ -0,0 +1,7 @@
+def saludoConParametros(nombre="Kevin"):
+    return "Hello World", nombre
+
+
+print(saludoConParametros())
+
+print(saludoConParametros("Juan"))
